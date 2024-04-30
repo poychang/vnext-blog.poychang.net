01b3e2cdb1fd9da6f17b9252203d3f6cf72a73d3
Poy Chang <poypost@gmail.com>
2024-04-30 13:45:27 +0800

    init project

---
+++ README.md
@@ -0,0 +1,21 @@
+# vnext-blog.poychang.net
+
+Base on Statiq .NET to generate static-site
+
+## Build
+
+After you have the files you can build the site using:
+
+```
+dotnet run
+```
+
+You can then access the site locally using:
+
+```
+http://localhost:5080/
+```
+
+## Useful Documentation
+
+- [Statiq website](https://www.statiq.dev/)
---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿using Statiq.Markdown;
+
+namespace Blog
+{
+    internal class Program
+    {
+        static async Task<int> Main(string[] args) =>
+            await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .AddSetting(MarkdownKeys.MarkdownExtensions, new List<string> { "Bootstrap", "Prism" })
+                .RunAsync();
+    }
+}
